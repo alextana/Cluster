5bb5c58cd9396f3cf0988699ddcd36746942ff29
Alex Tana <alextana@Alexs-MacBook-Air.local>
2022-10-23 00:02:53 +0100

    add hook to close popover on click outside

--- src/components/ui/popover/Popover.tsx
+++ src/components/ui/popover/Popover.tsx
@@ -1,6 +1,7 @@
 // Popover.tsx
 import * as PopoverPrimitive from "@radix-ui/react-popover";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import useOnClickOutside from "../../../hooks/useOnClickOutside";
 
 export const Popover = ({
   children,
@@ -18,10 +19,19 @@ export const Popover = ({
     setIsOpen(false);
   }
 
+  const ref = useRef(null);
+
+  const handleClickOutside = () => {
+    setIsOpen(false);
+  };
+
+  useOnClickOutside(ref, handleClickOutside);
+
   return (
     <PopoverPrimitive.Root open={isOpen}>
       <PopoverTrigger onClick={() => setIsOpen(true)}>{trigger}</PopoverTrigger>
       <PopoverContent
+        ref={ref}
         onClick={handleContentClick}
         className="mt-1 overflow-hidden rounded-md border border-white/10 bg-zinc-600/40 shadow-2xl backdrop-blur-md "
       >
